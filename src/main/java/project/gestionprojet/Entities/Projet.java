0e078ab8cee123c99f65eea2ae2e3cf30205be30
Ayoub <ayoubbarahal2@gmail.com>
2025-02-27 12:59:30 +0000

    ADDING Projet Service And Test
    # Conflicts:
    #	src/main/java/project/gestionprojet/Controllers/ProjetController.java
    #	src/main/java/project/gestionprojet/DTO/ProjetDTO.java
    #	src/main/java/project/gestionprojet/Repositories/ProjetRepo.java
    #	src/main/java/project/gestionprojet/ServiceImpl/ProjetServiceImpl.java
    #	src/test/java/project/gestionprojet/ProjetTest.java

--- src/main/java/project/gestionprojet/Entities/Projet.java
+++ src/main/java/project/gestionprojet/Entities/Projet.java
@@ -5,21 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class Projet {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idProjet;
     private String nomProjet;
 
-    @OneToMany
-    private List<ProductBacklog> productBacklogs;
 
     public Projet(String nomProjet) {
         this.nomProjet = nomProjet;
--- src/main/java/project/gestionprojet/Repositories/ProjetRepo.java
+++ src/main/java/project/gestionprojet/Repositories/ProjetRepo.java
@@ -1,12 +1,9 @@
 package project.gestionprojet.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import project.gestionprojet.Entities.Projet;
-
 
 
 public interface ProjetRepo extends JpaRepository<Projet, Integer> {
     Projet save(Projet projet);
-    Projet findByNomProjet(String projetName);
 }
