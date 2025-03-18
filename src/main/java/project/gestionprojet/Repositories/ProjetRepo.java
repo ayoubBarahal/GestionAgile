b4e22e938b728b6b40071fbddbd08ea15754a7e2
younesse <boualambg92@gmail.com>
2025-03-18 15:21:07 +0000

    staging changes to pull and rebase

--- src/main/java/project/gestionprojet/Repositories/ProjetRepo.java
+++ src/main/java/project/gestionprojet/Repositories/ProjetRepo.java
@@ -1,11 +1,9 @@
 package project.gestionprojet.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.gestionprojet.Entities.Projet;
 
-import java.util.List;
 
 @Repository
 public interface ProjetRepo extends JpaRepository<Projet, Integer> {
